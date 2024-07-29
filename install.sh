418ef5caf89141d4631258ca47d00e4d1d0f9cf3
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-07-29 17:17:58 +0100

    Update install.sh - Removed nodejs - uprated php8.2 for bookworm

--- install.sh
+++ install.sh
@@ -17,10 +17,6 @@ OP=/etc/svxlink
 cd
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 sudo apt update
 sudo apt upgrade -y
@@ -30,7 +26,7 @@ VERSIONS=svxlink/src/versions
 
 # Installing other packages
 	echo -e `date` " ${YELLOW} Installing required software packages${NORMAL}"
-	sudo apt install build-essential g++ make cmake libsigc++-2.0-dev php8.0 nodejs libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y
+	sudo apt install build-essential g++ make cmake libsigc++-2.0-dev php8.2 libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet -y
 	echo         
 	echo -e "${GREEN} Enter the node callsign: \n ${NORMAL}"
 	echo
